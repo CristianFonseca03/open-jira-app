ed4e2d2614e26b3eaac593ae6fe2c005822c967b
sergio1599 <sergioq15@hotmail.com>
2022-08-30 18:32:22 -0500

    add validations on the NewEntry form

--- components/ui/NewEntry.tsx
+++ components/ui/NewEntry.tsx
@@ -1,40 +1,72 @@
+import { ChangeEvent, useState } from "react";
 import { Button, Box, TextField } from "@mui/material";
 import SaveOutlinedIcon from "@mui/icons-material/SaveOutlined";
 import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
 import AddButton from "@mui/icons-material/AddCircleOutlineOutlined";
 
 export const NewEntry = () => {
+  const [isAdding, setisAdding] = useState(false);
+
+  const [inputValue, setInputValue] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
+
+  const onTextFieldChanged = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const onSave = () => {
+    if (inputValue.length === 0) return;
+    console.log(inputValue);
+  };
+
   return (
     <Box sx={{ marginBottom: 2, paddingX: 1 }}>
-      <Button endIcon={<AddButton />} fullWidth variant="outlined">
-        Agregar Tarea
-      </Button>
-
-      <TextField
-        fullWidth
-        sx={{ marginTop: 2, marginBottom: 1 }}
-        placeholder="Nueva entrada"
-        autoFocus
-        multiline
-        label="Nueva entrada"
-        helperText="Ingrese un valor"
-      />
-      <Box display="flex" justifyContent="space-between">
+      {isAdding ? (
+        <>
+          <TextField
+            fullWidth
+            sx={{ marginTop: 2, marginBottom: 1 }}
+            placeholder="Nueva entrada"
+            autoFocus
+            multiline
+            label="Nueva entrada"
+            helperText={
+              inputValue.length <= 0 && isTouched && "Ingrese un valor"
+            }
+            error={inputValue.length <= 0 && isTouched}
+            value={inputValue}
+            onChange={onTextFieldChanged}
+            onBlur={() => setIsTouched(true)}
+          />
+          <Box display="flex" justifyContent="space-between">
+            <Button
+              variant="outlined"
+              color="secondary"
+              endIcon={<CancelOutlinedIcon />}
+              onClick={() => setisAdding(false)}
+            >
+              Cancelar
+            </Button>
+            <Button
+              variant="outlined"
+              color="secondary"
+              endIcon={<SaveOutlinedIcon />}
+              onClick={onSave}
+            >
+              Guardar
+            </Button>
+          </Box>
+        </>
+      ) : (
         <Button
+          endIcon={<AddButton />}
+          fullWidth
           variant="outlined"
-          color="secondary"
-          endIcon={<CancelOutlinedIcon />}
+          onClick={() => setisAdding(true)}
         >
-          Cancelar
+          Agregar Tarea
         </Button>
-        <Button
-          variant="outlined"
-          color="secondary"
-          endIcon={<SaveOutlinedIcon />}
-        >
-          Guardar
-        </Button>
-      </Box>
+      )}
     </Box>
   );
 };
