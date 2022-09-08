4734627222591dd529610f1438f0582948e3c7be
sergio1599 <sergioq15@hotmail.com>
2022-09-08 17:30:19 -0500

    update entries in front from database

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -25,13 +25,21 @@ export const EntriesProvider: FC<PropsWithChildren> = ({ children }) => {
     }; */
 
     const { data } = await entriesApi.post<Entry>("/entries", {
-      description
+      description,
     });
     dispatch({ type: "[Entry] Add-Entry", payload: data });
   };
 
-  const updateEntry = (entry: Entry) => {
-    dispatch({ type: "[Entry] Entry-Updated", payload: entry });
+  const updateEntry = async ({ _id, description, status }: Entry) => {
+    try {
+      const { data } = await entriesApi.put<Entry>(`/entries/${_id}`, {
+        description,
+        status,
+      });
+      dispatch({ type: "[Entry] Entry-Updated", payload: data });
+    } catch (error) {
+      console.log({ error });
+    }
   };
 
   const refreshEntries = async () => {
--- pages/api/entries/index.ts
+++ pages/api/entries/index.ts
@@ -14,6 +14,8 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
             return getEntries(res);
         case 'POST':
             return postEntry(req, res);
+        case 'PUT':
+            return postEntry(req, res);
         default:
             return res.status(400).json({ message: 'Enpoint no existe' })
     }
